af0ee2c8576dad252a2dc1a89f1d571a7edb7c1d
Vladimir Hohrenko <vaxiktru@gmail.com>
2023-02-22 21:39:26 +0300

    task 12

---
+++ task12.py
@@ -0,0 +1,78 @@
+import pickle
+from copy import deepcopy
+from typing import TypeVar, Union, final
+
+T = TypeVar('T')
+
+
+class General(object):
+
+    def __init__(self, **kwargs) -> None: ...
+
+    @final
+    def copy(self, other: T) -> None:
+        other.__dict__ = deepcopy(self.__dict__)
+
+    @final
+    def clone(self) -> T:
+        return deepcopy(self)
+
+    @final
+    def serialize(self) -> bytes:
+        return pickle.dumps(self)
+
+    @final
+    @staticmethod
+    def deserialize(serialized_obj: bytes) -> T:
+        return pickle.loads(serialized_obj)
+
+    @final
+    def is_type(self, classtype) -> bool:
+        return type(self) == classtype
+
+    @final
+    def type(self):
+        return self.__class__.__name__
+
+    @final
+    def attributes(self) -> str:
+        return f'attributes: {self.__dict__}'
+
+    @final
+    def __eq__(self, other: T) -> bool:
+        return self.__dict__ == other.__dict__
+
+    @final
+    @staticmethod
+    def assignment_attempt(target: 'General',
+                           source: 'General') -> Union['General', 'NoneType']:
+        if target.is_type(source.type()):
+            return source
+        return NoneType()
+
+
+class Any(General):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.__dict__ = kwargs
+
+
+class A(Any):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
+class B(Any):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+
+@final
+class NoneType(A, B):
+
+    @final
+    def __new__(cls, **kwargs):
+        return None
